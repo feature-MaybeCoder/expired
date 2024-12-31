177e13dc5916707b88175fd15c5068b4d46870a7
lastdarknes <tim.m@creek.art>
2025-01-01 02:30:44 +0700

    add exception_handler and firebase init to setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,11 @@
 import fastapi as fa
 from app.api import router
+from app.config import settings
+from app.exceptions_handlers import handle_exceptions
+from app.firebase import init_firebase_app
+
+init_firebase_app(settings.FIREBASE_CERTS)
 
 fa_app = fa.FastAPI()
 fa_app.include_router(router)
+fa_app.add_exception_handler(Exception, handle_exceptions)
