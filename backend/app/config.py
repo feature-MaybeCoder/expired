330b8583855a79ba31d0492a226bbbd3280903c1
lastdarknes <tim.m@creek.art>
2025-01-01 02:17:17 +0700

    add security info to config

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,8 +6,16 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    # Security
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
     # Database
     DB_URI: str
+
+    # Firebase
+    FIREBASE_CERTS: dict
 
 
 settings = Settings()
