2d3d452396b85360abfc40893ec64473d25660b8
lastdarknes <tim.m@creek.art>
2025-01-04 15:18:50 +0700

    add auth header

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,8 +1,14 @@
 import { createClient } from '@hey-api/client-fetch';
+import { store } from "@/redux/store";
 
+const current_state = store.getState()
 
 export const apiClient = createClient({
-        baseUrl: 'http://0.0.0.0:8010',
-        
-    });
+            baseUrl: 'http://0.0.0.0:8010',
+        }
+    );
 
+apiClient.interceptors.request.use((request, options) => {
+request.headers.set('Authorization', 'Bearer ' + current_state.accessToken );
+return request;
+});
