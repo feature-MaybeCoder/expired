7e4f703cfc7285d46e2253b8d6dcf6157db8994c
lastdarknes <tim.m@creek.art>
2025-01-06 18:12:30 +0700

    add test root page

--- frontend/src/pages/root/page.tsx
+++ frontend/src/pages/root/page.tsx
@@ -1,10 +1,50 @@
+import { apiClient } from "@/api/client";
+import { testAuthApiV1AuthPasswordTestGet } from "@/api/sdk.gen";
+import { selectAccessToken } from "@/reducers/token";
+import { store } from "@/redux/store";
+import { useState } from "react";
+
 
 export default function RootPage() {
-  return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <h1>RootPage</h1>
-      </div>
-    </div>
-  )
+    const state = testAuthApiV1AuthPasswordTestGet(
+        {
+            client: apiClient
+        })
+
+    const [dummyState, dummySetState] = useState("Initial state")
+    
+    let display = "Error"
+    state.then((value) => {
+        if (value.response.status != 401) {
+            const state = store.getState()
+            const accessToken = selectAccessToken(
+                state
+            )
+
+            display = accessToken.accessToken
+        } else{
+            display = "Problem with token"
+        }
+        
+    })
+    state.catch((error: Error) => {
+        display = error.message
+        console.log(error)
+    })
+    state.finally(() => {
+        if (display) {
+            dummySetState(display)
+        }
+    })
+    
+    
+    
+    return (
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+        <div className="w-full max-w-sm">
+            <h1>{dummyState}</h1>
+            
+        </div>
+        </div>
+    )
 }
