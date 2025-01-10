2a8bc02a526fdbfe18f17f9e86e4bcf78e7e75a1
lastdarknes <tim.m@creek.art>
2025-01-10 15:35:23 +0700

    refactor RegisterForm

--- frontend/src/components/register-form.tsx
+++ frontend/src/components/register-form.tsx
@@ -3,125 +3,130 @@ import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
-  CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { FormEvent, useState } from "react"
-import { passwordRegistrationApiV1AuthPasswordRegisterPost } from "@/api/index";
+import { ChangeEvent, FormEvent, useState } from "react"
 import { useNavigate } from "react-router-dom";
 import { ROOT_ROUTES } from "@/constants/routes/root"
-import {
-  Alert,
-  AlertDescription,
-  AlertTitle,
-} from "@/components/ui/alert"
-import { apiClient } from "@/api/client";
+import { registrationService } from "@/services/register"
+import { EmailInput } from "./ui/email_input"
+import { CardDescriptionWithError } from "./ui/card_description_with_error"
+import { validatePasswordForRegister } from "@/utils/validation"
+
+import { LOGIN_ROUTES } from "@/constants/routes/login"
+
+const defaultPasswordErrorText = "^(?=.*[a-z]): Ensures at least one lowercase letter. \n \
+(?=.*[A-Z]): Ensures at least one uppercase letter. \n \
+(?=.*d): Ensures at least one digit. \n \
+(?=.*[@$!%*?&]): Ensures at least one special character. \n \
+[A-Za-zd@$!%*?&]{8,}$: Ensures the password is at least 8 characters long. \n \
+"
 
 export function RegisterForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
+  const [errorState, errorChangeState] = useState(false)
+  const [descriptionState, descriptionChangeState] = useState("Enter your email below to create your account.")
   const [emailState, emailChangeState] = useState("")
-  const [passwordState, passwordChangeState] = useState("")
-  const [registerFormState, setRegisterFormState] = useState({
-      errorMessage: ""
+  const [passwordState, passwordChangeState] = useState({
+      value: "",
+      errorText: "",
+      isError: false
     }
-  )
-  const navigate = useNavigate()
-  let displayError = (<div></div>)
+)
 
+  const navigate = useNavigate()
+
+  const onPasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+      const isPasswordValid = validatePasswordForRegister(event.target.value)
+      passwordChangeState(
+        {
+          value: event.target.value,
+          isError: !isPasswordValid,
+          errorText: defaultPasswordErrorText
+        }
+      )
+  }
   const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    const response = await passwordRegistrationApiV1AuthPasswordRegisterPost(
-      {
-        client: apiClient,
-        method: "POST",
-        body: 
-          {
-            email: emailState,
-            raw_password: passwordState
-          }
-      }
+    const registrationAttempt = registrationService.registerWithEmailAndPassword(
+      emailState,
+      passwordState.value
     )
 
-
-    if (response.error){
-      let msg = "Unexpected error"
-      if (response.error.detail) {
-        msg = response.error.detail.toString()
-      } else {
-        
-        console.error(response.error)
-      }
-      setRegisterFormState({
-        errorMessage: msg
-      })
-      
-    } else {
-      console.log(response.data.access_token)
-      console.log("Successful")
+    registrationAttempt.then(() => {
       navigate(ROOT_ROUTES.root)
     }
-}
-
-  if (registerFormState.errorMessage) {
-    displayError = (
-      <Alert>
-        <AlertTitle className="left">Error!</AlertTitle>
-        <AlertDescription>
-          {registerFormState.errorMessage}
-        </AlertDescription>
-      </Alert>
+    )
+    registrationAttempt.catch((error) => {
+      errorChangeState(true)
+      descriptionChangeState(
+        error
+      )
+    }
     )
   }
 
-  return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-2xl">Sign up</CardTitle>
-          {displayError}
-          <CardDescription>
-            Enter your email below to create your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={onSubmit}>
+return (
+  <div className={cn("flex flex-col gap-6", className)} {...props}>
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-2xl">Sign up</CardTitle>
+        <CardDescriptionWithError isError={errorState}>
+            {descriptionState}
+        </CardDescriptionWithError>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={onSubmit}>
           <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  value={emailState}
-                  onChange={(e) => {emailChangeState(e.target.value) }}
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                <Input
-                value={passwordState}
-                onChange={(e) => {passwordChangeState(e.target.value) }}
-                id="password"
-                type="password" 
-                required />
-              </div>
-              <Button className="w-full">
-                Sign up
-              </Button>
-              <Button variant="outline" className="w-full">
-              Sign up with Google
-              </Button>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <EmailInput
+              className=""
+              value={emailState}
+              onChange={(e) => {emailChangeState(e.target.value)}}
+              id="email"
+              type="email"
+              placeholder="email@example.com"
+              required
+            />
             </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input
+              isError={passwordState.isError}
+              errorMessage={""}
+              value={passwordState.value}
+              onChange={onPasswordChange}
+              id="password"
+              type="password"
+              required 
+              />
             </div>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
-  )
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+            <Button variant="outline" className="w-full">
+              Login with Google
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <a href={LOGIN_ROUTES.login} className="underline underline-offset-4">
+              Login
+            </a>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  </div>
+)
+
 }
+
