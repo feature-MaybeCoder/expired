154f5e094a8a19c2614c76863df4cd36d8089c37
lastdarknes <tim.m@creek.art>
2025-01-10 17:07:28 +0700

    remove mock funcional from RootPage

--- frontend/src/pages/root/page.tsx
+++ frontend/src/pages/root/page.tsx
@@ -1,50 +1,16 @@
-import { apiClient } from "@/api/client";
-import { testAuthApiV1AuthPasswordTestGet } from "@/api/sdk.gen";
-import { selectAccessToken } from "@/reducers/token";
-import { store } from "@/redux/store";
-import { useState } from "react";
-
+import { ABOUT_ROUTES } from "@/constants/routes/about"
+import { DASHBOARD_ROUTES } from "@/constants/routes/dashboard"
+import { firebaseAuthApp } from "@/firebase"
 
 export default function RootPage() {
-    const state = testAuthApiV1AuthPasswordTestGet(
-        {
-            client: apiClient
-        })
+    if (firebaseAuthApp.currentUser) {
+        location.replace(DASHBOARD_ROUTES.dashboard)
+    } else {
+        location.replace(ABOUT_ROUTES.about)
+    }
 
-    const [dummyState, dummySetState] = useState("Initial state")
-    
-    let display = "Error"
-    state.then((value) => {
-        if (value.response.status != 401) {
-            const state = store.getState()
-            const accessToken = selectAccessToken(
-                state
-            )
-
-            display = accessToken.accessToken
-        } else{
-            display = "Problem with token"
-        }
-        
-    })
-    state.catch((error: Error) => {
-        display = error.message
-        console.log(error)
-    })
-    state.finally(() => {
-        if (display) {
-            dummySetState(display)
-        }
-    })
-    
-    
-    
     return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-        <div className="w-full max-w-sm">
-            <h1>{dummyState}</h1>
-            
-        </div>
         </div>
     )
 }
