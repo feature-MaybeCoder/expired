0fb69eec0ec5c37506f79e868631ca308dadfc22
lastdarknes <tim.m@creek.art>
2025-01-10 15:35:33 +0700

    refactor login form

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -3,32 +3,75 @@ import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
-  CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { REGISTER_ROUTES } from "@/constants/routes/register"
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router"
+import { loginService } from "@/services/login"
+import { CardDescriptionWithError } from "./ui/card_description_with_error"
+import { EmailInput } from "./ui/email_input"
+
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
+  const [emailState, setEmailState] = useState("")
+  const [passwordState, setPasswordState] = useState("")
+  const [descriptionState, descriptionChangeState] = useState("Enter your email below to login to your account.")
+  const [errorState, errorChangeState] = useState(false)
+  const navigate = useNavigate()
+
+  const login = (email: string, raw_password: string) => {
+    const tryLogin = loginService.login(email, raw_password)
+
+    try {
+      console.log("Trying to login")
+      tryLogin.then((loginData) => {
+        if (loginData.redirectPath) {
+          navigate(loginData.redirectPath)
+        }
+      })
+    } catch (error: unknown) {
+      console.log("Login error")
+      errorChangeState(true)
+      if (error instanceof Error) {
+        descriptionChangeState(error.message)
+      } else (
+        descriptionChangeState("Unexpected error!")
+      )
+      console.error(error)
+    }
+
+  }
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    login(emailState, passwordState)
+
+  }
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
           <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
+          <CardDescriptionWithError isError={errorState}>
+            {descriptionState}
+          </CardDescriptionWithError>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={onSubmit}>
             <div className="flex flex-col gap-6">
               <div className="grid gap-2">
                 <Label htmlFor="email">Email</Label>
-                <Input
+                <EmailInput
+                  className=""
+                  value={emailState}
+                  onChange={(e) => {setEmailState(e.target.value)}}
                   id="email"
                   type="email"
                   placeholder="m@example.com"
@@ -45,7 +88,15 @@ export function LoginForm({
                     Forgot your password?
                   </a>
                 </div>
-                <Input id="password" type="password" required />
+                <Input
+                isError={false}
+                errorMessage={""}
+                onChange={(e) => {setPasswordState(e.target.value)}}
+                value={passwordState}
+                id="password"
+                type="password"
+                required 
+                />
               </div>
               <Button type="submit" className="w-full">
                 Login
@@ -56,7 +107,7 @@ export function LoginForm({
             </div>
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
+              <a href={REGISTER_ROUTES.register} className="underline underline-offset-4">
                 Sign up
               </a>
             </div>
