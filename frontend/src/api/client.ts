6078ca3991152dc67cb8c8f63a10dbb869c0ba17
lastdarknes <tim.m@creek.art>
2025-01-10 15:39:37 +0700

    update client

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,14 +1,43 @@
 import { createClient } from '@hey-api/client-fetch';
-import { store } from "@/redux/store";
+import { authService } from '@/services/auth';
+import { getAuth } from 'firebase/auth';
+import { BEARER_AUTHORIZATION_PREFIX, HEADERS } from '@/constants/http';
 
-const current_state = store.getState()
 
 export const apiClient = createClient({
             baseUrl: 'http://0.0.0.0:8010',
+
         }
     );
 
-apiClient.interceptors.request.use((request, options) => {
-request.headers.set('Authorization', 'Bearer ' + current_state.accessToken );
-return request;
+apiClient.interceptors.response.use((response) => {
+    if (
+        response.status == 401
+
+    ){
+        authService.processNotLoggedInUser()
+    }
+    
+    return response;
+});
+
+apiClient.interceptors.request.use((request) => {
+    const currentUser = getAuth().currentUser
+    let accessToken = ""
+    if (currentUser){
+        const getIdToken = currentUser.getIdToken()
+        getIdToken.then((idToken) => {
+            accessToken = idToken
+            console.log("Got token", idToken)
+        })
+        getIdToken.catch((error) => {
+            console.error("Error getting id token: ", error)
+        })
+    }
+
+    request.headers.set(
+        HEADERS.authorization, 
+        BEARER_AUTHORIZATION_PREFIX + accessToken
+    )
+    return request;
 });
