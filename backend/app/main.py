dc0f57f599c66b40ae742964899bea9f8a9d827a
lastdarknes <tim.m@creek.art>
2025-01-10 20:49:21 +0700

    add cors middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
 import fastapi as fa
+from starlette.middleware.cors import CORSMiddleware
+
 from app.api import router
 from app.config import settings
 from app.exceptions_handlers import handle_exceptions
@@ -8,4 +10,7 @@ init_firebase_app(settings.FIREBASE_CERTS)
 
 fa_app = fa.FastAPI()
 fa_app.include_router(router)
+fa_app.add_middleware(
+    CORSMiddleware, allow_origins=["*"],  allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
 fa_app.add_exception_handler(Exception, handle_exceptions)
