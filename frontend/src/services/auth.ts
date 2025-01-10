5797ed87b3b279cf7f2836c86a01ae35f8efcf3c
lastdarknes <tim.m@creek.art>
2025-01-10 19:09:45 +0700

    update auth service

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -1,15 +1,29 @@
 import { AUTH_ROUTES, ROUTES_ALLOWED_WITHOUT_LOGIN } from "@/constants/auth"
+import { DASHBOARD_ROUTES } from "@/constants/routes/dashboard"
 import { LOGIN_ROUTES } from "@/constants/routes/login"
-import { ROOT_ROUTES } from "@/constants/routes/root"
-import { sessionAccessTokenName } from "@/constants/security"
+import { sessionAccessTokenName, sessionCookieLifetimeDays, sessionCookieName } from "@/constants/security"
+import Cookies from "universal-cookie"
+
+
+const cookies = new Cookies()
+
 
 class AuthService{
+    /**
+     * getCurrentUser
+     */
+    public isCurrentUserSignedIn() {
+        
+        return cookies.get(sessionCookieName) != null
+    }
 
     /**
      * processNotLoggedInUser
      */
     public processNotLoggedInUser() { 
+        console.warn("User wasnt log in")
         localStorage.removeItem(sessionAccessTokenName)
+        cookies.remove(sessionCookieName)
         
         if (location.pathname !in ROUTES_ALLOWED_WITHOUT_LOGIN) {
             location.replace(LOGIN_ROUTES.login)
@@ -19,10 +33,22 @@ class AuthService{
     /**
      * processUserLoggedIn
      */
-    public processUserLoggedIn(accessToken: string) {
+    public processUserLoggedIn(accessToken: string, refreshToken: string) {
+        const now = new Date()
+        const exporationDate = new Date()
+        exporationDate.setDate(now.getDate() + sessionCookieLifetimeDays)
         localStorage.setItem(sessionAccessTokenName, accessToken)
-        if (location.pathname !in AUTH_ROUTES) {
-            location.replace(ROOT_ROUTES.root)
+        cookies.set(
+            sessionCookieName,
+            refreshToken,
+            {
+                expires: exporationDate,
+                path: '/'
+            }
+        )
+
+        if (location.pathname in AUTH_ROUTES) {
+            location.replace(DASHBOARD_ROUTES.dashboard)
         }
     }
 }
