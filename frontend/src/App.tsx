7a4ffe975f9d00e3754b6f188f3b21ea49bed872
lastdarknes <tim.m@creek.art>
2025-01-10 15:40:57 +0700

    update app init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import RegisterPage from "@/pages/register/page"
 import RootPage from "@/pages/root/page"
 import { REGISTER_ROUTES } from '@/constants/routes/register'
 import { ROOT_ROUTES } from './constants/routes/root'
+import { LOGIN_ROUTES } from './constants/routes/login'
+import { Provider } from 'react-redux';
+import { store } from "@/redux/store";
+import LoginPage from './pages/login/page'
 
 function App() {
   const router = createBrowserRouter([
@@ -15,12 +18,18 @@ function App() {
     {
       path: ROOT_ROUTES.root,
       element: <RootPage/>
+    },
+    {
+      path: LOGIN_ROUTES.login,
+      element: <LoginPage/>
     }
   ])
 
   return (
     <>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </>
   )
 }
