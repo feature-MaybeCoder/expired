1889d86eca7958993b83fdd08468d0da5624718d
lastdarknes <tim.m@creek.art>
2025-01-10 19:13:38 +0700

    add new pages to init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,10 @@ import { LOGIN_ROUTES } from './constants/routes/login'
 import { Provider } from 'react-redux';
 import { store } from "@/redux/store";
 import LoginPage from './pages/login/page'
+import { DASHBOARD_ROUTES } from './constants/routes/dashboard'
+import DashboardPage from './pages/dashboard/page'
+import { ABOUT_ROUTES } from './constants/routes/about'
+import AboutPage from './pages/about/page'
 
 function App() {
   const router = createBrowserRouter([
@@ -22,7 +26,15 @@ function App() {
     {
       path: LOGIN_ROUTES.login,
       element: <LoginPage/>
-    }
+    },
+    {
+      path: DASHBOARD_ROUTES.dashboard,
+      element: <DashboardPage/>
+    },
+    {
+      path: ABOUT_ROUTES.about,
+      element: <AboutPage/>
+    },
   ])
 
   return (
