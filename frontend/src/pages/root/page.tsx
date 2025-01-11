fe9d3e11db00a89147661808ffe5322b52453481
lastdarknes <tim.m@creek.art>
2025-01-11 22:25:15 +0700

    refactor root page

--- frontend/src/pages/root/page.tsx
+++ frontend/src/pages/root/page.tsx
@@ -1,16 +1,16 @@
 import { ABOUT_ROUTES } from "@/constants/routes/about"
 import { DASHBOARD_ROUTES } from "@/constants/routes/dashboard"
 import { authService } from "@/services/auth"
+import { useEffect } from "react"
 
 export default function RootPage() {
-    if (authService.isCurrentUserSignedIn()) {
-        location.replace(DASHBOARD_ROUTES.dashboard)
-    } else {
-        location.replace(ABOUT_ROUTES.about)
-    }
-
-    return (
-        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-        </div>
-    )
+    useEffect(() => {
+        if (authService.isCurrentUserSignedIn()) {
+            location.replace(DASHBOARD_ROUTES.dashboard)
+        } else {
+            location.replace(ABOUT_ROUTES.about)
+        }
+    }, [])
+    
+    return <></>
 }
