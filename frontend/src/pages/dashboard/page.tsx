579d37b5663154c4be8fd7bb804a6816de922d1b
lastdarknes <tim.m@creek.art>
2025-01-11 22:28:40 +0700

    refactor dashboard page

--- frontend/src/pages/dashboard/page.tsx
+++ frontend/src/pages/dashboard/page.tsx
@@ -1,24 +1,35 @@
 import { testAuthApiV1AuthPasswordTestGet } from "@/api"
 import { apiClient } from "@/api/client"
+import { ButtonWithUrl } from "@/components/ui/button_with_url"
 import { Label } from "@/components/ui/label"
+import { LOGOUT_ROUTES } from "@/constants/routes/logout"
+import { useEffect } from "react"
 
 export default function DashboardPage() {
-    const test = testAuthApiV1AuthPasswordTestGet(
-        {
-            client: apiClient,
-            method: "GET"
+    useEffect(() => {
+        const test = testAuthApiV1AuthPasswordTestGet(
+            {
+                client: apiClient,
+                method: "GET"
+            }
+        )
+        test.then(() => {
+            console.log("success")
         }
-    )
-    test.then(() => {
-        console.log("success")
-    }
-    )
-    test.catch((error) => {
-        console.error(error)
-    }) 
+        )
+        test.catch((error) => {
+            console.error(error)
+        }) 
+    })
     return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-            <Label>A dashboard for authenticated users</Label>
+            <div className="grid gap-9">
+                <Label>A dashboard for authenticated users</Label>
+                <ButtonWithUrl url={LOGOUT_ROUTES.logout}>
+                    Logout
+                </ButtonWithUrl>
+            </div>
+            
         </div>
     )
 }
