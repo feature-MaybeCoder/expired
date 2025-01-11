186426af2987899137fced0f4339a94dba2c68cd
lastdarknes <tim.m@creek.art>
2025-01-11 22:29:13 +0700

    update ROUTES_ALLOWED_WITHOUT_LOGIN

--- frontend/src/constants/auth.ts
+++ frontend/src/constants/auth.ts
@@ -2,10 +2,11 @@ import { Tuple } from "@reduxjs/toolkit";
 import { ROOT_ROUTES } from "./routes/root";
 import { REGISTER_ROUTES } from "./routes/register";
 import { LOGIN_ROUTES } from "./routes/login";
+import { ABOUT_ROUTES } from "./routes/about";
 
 export const ROUTES_ALLOWED_WITHOUT_LOGIN = new Tuple(
     [
-        ROOT_ROUTES.root, REGISTER_ROUTES.register, LOGIN_ROUTES.login
+        ROOT_ROUTES.root, REGISTER_ROUTES.register, LOGIN_ROUTES.login, ABOUT_ROUTES.about
     ]
 )
 
