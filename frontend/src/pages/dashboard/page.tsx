4e69d1c9db0b24d25d2f296fd787506d50c53d8c
lastdarknes <tim.m@creek.art>
2025-01-11 23:25:37 +0700

    add user widget

--- frontend/src/pages/dashboard/page.tsx
+++ frontend/src/pages/dashboard/page.tsx
@@ -1,11 +1,23 @@
 import { testAuthApiV1AuthPasswordTestGet } from "@/api"
 import { apiClient } from "@/api/client"
+import { AuthContext } from "@/components/auth/auth-context"
 import { ButtonWithUrl } from "@/components/ui/button_with_url"
 import { Label } from "@/components/ui/label"
 import { LOGOUT_ROUTES } from "@/constants/routes/logout"
-import { useEffect } from "react"
+import { useContext, useEffect } from "react"
 
 export default function DashboardPage() {
+    const authContext = useContext(AuthContext)
+    let userWidget = <></>
+    if (authContext && authContext.user) {
+        userWidget = (
+            <div>
+                <Label>Your account:</Label>
+                <p>{authContext.user.email}</p>
+            </div>
+        )
+    }
+    
     useEffect(() => {
         const test = testAuthApiV1AuthPasswordTestGet(
             {
@@ -24,6 +36,8 @@ export default function DashboardPage() {
     return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
             <div className="grid gap-9">
+                {userWidget}
+
                 <Label>A dashboard for authenticated users</Label>
                 <ButtonWithUrl url={LOGOUT_ROUTES.logout}>
                     Logout
