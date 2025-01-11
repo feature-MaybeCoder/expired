a843e06893759cf90f690c5d4866c05a9d0d90bb
lastdarknes <tim.m@creek.art>
2025-01-11 22:28:54 +0700

    refactor about page

--- frontend/src/pages/about/page.tsx
+++ frontend/src/pages/about/page.tsx
@@ -1,13 +1,21 @@
+import { ButtonWithUrl } from "@/components/ui/button_with_url";
 import { Label } from "@/components/ui/label";
 import { LOGIN_ROUTES } from "@/constants/routes/login";
+import { REGISTER_ROUTES } from "@/constants/routes/register";
 
 export default function AboutPage() {
 
     return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-            <div className="align-[4px]">
-                <Label>A page for not authenticated users</Label>
-                <a className="border-s-black border-4" href={LOGIN_ROUTES.login}>Login</a>
+            <div className="grid gap-4">
+                <Label className="mb-3">A page for not authenticated users</Label>
+                <ButtonWithUrl url={LOGIN_ROUTES.login}>
+                    Login
+                </ButtonWithUrl>
+                    
+                <ButtonWithUrl url={REGISTER_ROUTES.register}>
+                    Register
+                </ButtonWithUrl>  
             </div>
             
         </div>
